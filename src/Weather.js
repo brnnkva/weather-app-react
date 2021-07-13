cf79b0dbdf62688991e5c980e3796c7abfaeb074
brnnkva <64192884+brnnkva@users.noreply.github.com>
2021-07-13 12:27:26 +0200

    added html structure for Weather

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,40 @@ import "./Weather.css";
 export default function Weather(){
     return (
         <div className="Weather">
-            <h1>Weather</h1>
+            <div className="row">
+                
+                <div className="col-6 top-left">
+                      <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny>" className="weather-icon"/>
+                    <span className="main-temperature"> 29°C</span>
+                    <div className="weather-details">
+                      <ul>
+                         <li>
+                             Description: sunny
+                         </li>
+                         <li>
+                             Humidity: 50%
+                         </li>
+                         <li>
+                             Wind: 10 km/h
+                         </li> 
+                     </ul>
+                  </div>
+               </div>
+                
+                <div className="col-6 top-right">
+                    <h3>Budapest</h3>
+                    <ul>
+                        <li>
+                            Tuesday, July 13
+                        </li>
+                        <li>
+                            12:30
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+
         </div>
     )
 }
