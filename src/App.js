1e206403d034b52e815c8469eeba03f7acb22c73
brnnkva <64192884+brnnkva@users.noreply.github.com>
2021-07-13 11:45:11 +0200

    added Weather component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <div>Weather</div>
-  
- 
+      <Weather />
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather(){
+    return (
+        <div className="Weather">
+            <h1>Weather</h1>
+        </div>
+    )
+}
