09102669fea4ae1fb282f3578d50e9cf4a6a3ee6
brnnkva <64192884+brnnkva@users.noreply.github.com>
2021-08-03 20:02:11 +0200

    created forecast day component

---
+++ src/ForecastDay.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+export default function ForecastDay(props){
+
+  function day(){
+      let date = new Date(props.forecast.dt * 1000);
+      let day = date.getDay();
+      let days =  [
+        "Sun",
+        "Mon",
+       "Tue", 
+       "Wed", 
+       "Thu", 
+       "Fri",
+       "Sat"
+       ];
+       return days[day];
+  }  
+
+  function maxTemp(){
+      let temperature= Math.round(props.forecast.temp.max);
+      return `${temperature}°`;
+  }
+
+  function minTemp(){
+    let temperature= Math.round(props.forecast.temp.min);
+    return  `${temperature}°`;
+}
+
+    return (
+        <div className="ForecastDay">
+            <div className="WeatherForecast">
+                <div className="row">
+                    <div className="col">
+                        <div>
+                            {day()}
+                        </div>
+                        <div className="forecast-img">
+                        <img src={props.icon.url} alt={props.icon.description}/>
+                        </div>
+                        <span className="forecast-temp-max">
+                          {maxTemp()}
+                        </span>
+                        <span className="forecast-temp-min">
+                        {minTemp()}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,37 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
+import ForecastDay from "./ForecastDay";
 import axios from "axios";
 
 export default function WeatherForecast(props){
-
+ let [loaded, setLoaded] = useState(false);
+ let [forecastData, setForecastData] = useState(null);
+ let [icon, setIcon] =useState(" ");
+ 
     function handleResponse(response){
-        console.log(response.data);
-    }
+       setForecastData(response.data.daily);
+       setIcon({
+            url: `http://openweathermap.org/img/wn/${response.data.daily[0].weather[0].icon}@2x.png`,
+            description: response.data.daily[0].weather[0].icon
+        });
+        setLoaded(true);
+    };
+
+    if (loaded){
+        return (
+            <ForecastDay forecast={forecastData[0]} icon={icon}/>
+        );
+    } else {
         let apiKey="a2448133104335b630f878b5541b3167";
         let longitude=props.coords.lon;
         let latitude=props.coords.lat;
-       let  apiUrl= `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+        let  apiUrl= `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     
-        axios.get(apiUrl).then(handleResponse);
-
-    return (
-        <div className="WeatherForecast">
-            <div className="row">
-                <div className="col">
-                    <div className="forecast-hour">
-                        14:00
-                    </div>
-                    <div className="forecast-img">
-                        <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="rain"/>
-                    </div>
-                    <span className="forecast-temp-max">
-                        19
-                    </span>
-                    <span className="forecast-temp-min">
-                        10
-                    </span>
-                </div>
-            </div>
-        </div>
-    );
+     axios.get(apiUrl).then(handleResponse);
+     return null;
+    }
+   
 }
